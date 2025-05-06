0b252ef01f4eb6ce82bcc5209a220a0107d4297c
Praveen Rapuru <praveenrapuru2002@gmail.com>
2025-05-06 21:05:34 +0530

    change dark mode button

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export default function Header({ darkMode, setDarkMode }) {
   const [activeLink, setActiveLink] = useState("");
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  const linkClass = " hover:bg-blue-500 px-4 py-2 text-sm rounded-md font-light";
+  const linkClass = "hover:bg-blue-500 px-4 py-2 text-sm rounded-md font-light";
 
   const handleClick = (link) => {
     setActiveLink(link);
@@ -15,24 +15,31 @@ export default function Header({ darkMode, setDarkMode }) {
   };
 
   return (
-    <header className={`bg-blue-600 dark:bg-gray-800 text-white shadow-md`}>
-      <div className="flex items-center justify-between px-4 py-4 md:px-4 md:py-4">
-        <div className="md:hidden flex items-center">
-          <button onClick={() => setIsSidebarOpen(true)} className="text-white">
-            <span className="material-icons">menu</span>
-          </button>
-          <div className="text-xl font-light ml-3">Loan Calculator</div>
-        </div>
-        <button
-              onClick={() => setDarkMode(!darkMode)}
-              className="ml-4 p-2 rounded bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600"
-            >
-              {darkMode ? <SunIcon className="h-5 w-5 text-yellow-400" /> : <MoonIcon className="h-5 w-5 text-blue-500" />}
+    <header className="bg-blue-600 dark:bg-gray-800 text-white shadow-md">
+      <div className="flex items-center justify-between px-4 py-4 md:px-4 md:py-3">
+        
+        <div className="md:hidden flex justify-between items-center w-full">
+          <div className="flex items-center">
+            <button onClick={() => setIsSidebarOpen(true)} className="text-white">
+              <span className="material-icons">menu</span>
             </button>
-       
+            <div className="text-xl font-light ml-3">Loan Calculator</div>
+          </div>
+          <button
+            onClick={() => setDarkMode(!darkMode)}
+            className="ml-4 p-2 rounded bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600"
+          >
+            {darkMode ? (
+              <SunIcon className="h-5 w-5 text-yellow-400" />
+            ) : (
+              <MoonIcon className="h-5 w-5 text-blue-500" />
+            )}
+          </button>
+        </div>
+
+        
         <div className="hidden md:flex items-center justify-between w-full">
           <div className="text-xl font-light">Loan Calculator</div>
-
           <div className="flex items-center space-x-8">
             <Link
               to="/"
@@ -62,17 +69,19 @@ export default function Header({ darkMode, setDarkMode }) {
             >
               ERROR
             </Link>
-
             <button
               onClick={() => setDarkMode(!darkMode)}
               className="ml-4 p-2 rounded bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600"
             >
-              {darkMode ? <SunIcon className="h-5 w-5 text-yellow-400" /> : <MoonIcon className="h-5 w-5 text-blue-500" />}
+              {darkMode ? (
+                <SunIcon className="h-5 w-5 text-yellow-400" />
+              ) : (
+                <MoonIcon className="h-5 w-5 text-blue-500" />
+              )}
             </button>
           </div>
         </div>
       </div>
-
       <MobileSidebar
         isSidebarOpen={isSidebarOpen}
         setIsSidebarOpen={setIsSidebarOpen}
